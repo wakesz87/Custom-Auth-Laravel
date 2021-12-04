aa2100e8f34ba28778dede0deb904c9e664f94d0
wakesz87 <kisszab87@gmail.com>
2021-12-04 19:02:18 +0100

    update

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class CustomAuthController extends Controller
 {
@@ -23,17 +24,35 @@ class CustomAuthController extends Controller
 
      public function registerUser(Request $request)
      {
-         $request->validate([
-              'username'=>'required', 
-              'email'=>'required',
-              'password'=>'required',
-              'lastname'=>'required',
-              'firstname'=>'required',
-              'birthdate'=>'required',
-              'birthplace'=>'required',
-              'city'=>'required',
-              'postalcode'=>'required',
-              'adress'=>'required'
-         ]);
+          $request->validate([
+               'username' => 'required|unique:users',
+               'email' => 'required|email',
+               'password' => 'required|min:5|max:12',
+               'lastname' => 'required|max:25',
+               'firstname' => 'required|max:25',
+               'birthdate' => 'required|date_format:Y-m-d|after_or_equal:1920-01-01',
+               'birthplace' => 'required|max:25',
+               'city' => 'required|max:25',
+               'postalcode' => 'required|max:10',
+               'adress' => 'required|max:25'
+          ]);
+
+          $user = new User();
+          $user->username = $request->username;
+          $user->email = $request->email;
+          $user->password = $request->password;
+          $user->lastname = $request->lastname;
+          $user->firstname = $request->firstname;
+          $user->birthdate = $request->birthdate;
+          $user->birthplace = $request->birthplace;
+          $user->city = $request->city;
+          $user->postalcode = $request->postalcode;
+          $user->adress = $request->adress;
+          $res = $user->save();
+          if($res){
+               return back()->with('success','A regisztrációd sikerült!');
+          }else{
+               return back()->with('failed','A regisztrációd sikertelen!');
+          }
      }
 }
--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>Bejelentkezés</title>
+    <title>Regisztráció</title>
 </head>
 
 <body>
@@ -16,73 +16,81 @@
                 <h3>Regisztráció</h3>
                 <hr>
                 <form action="{{route('register-user')}}" method="post">
-                    @csrf
 
+                    @if(Session::has('success'))
+                    <div class="alert alert-sucess">{{Session::get('success')}}</div>
+                    @endif
+                    @if(Session::has('failed'))
+                    <div class="alert alert-danger">{{Session::get('failed')}}</div>
+                    @endif
+
+
+                    @csrf
                     <div class="form-group">
                         <label for="username">Felhasználónév</label>
-                        <input type="text" class="form-control" placeholder="Add meg a Felhasználóneved" name="name" value="{{old('name')}}">
+                        <input type="text" class="form-control" placeholder="Add meg a Felhasználóneved" name="username" value="{{old('username')}}">
                         <span class="text-danger">@error('username') {{$message}} @enderror</span>
                     </div>
 
-                    
+
                     <div class="form-group">
                         <label for="email">Email cím</label>
-                        <input type="email" class="form-control" placeholder="Add meg az Email címed" name="email" value="">
+                        <input type="text" class="form-control" placeholder="Add meg az Email címed" name="email" value="{{old('email')}}">
                         <span class="text-danger">@error('email') {{$message}} @enderror</span>
                     </div>
 
                     <div class="form-group">
                         <label for="password">Jelszó</label>
-                        <input type="password" class="form-control" placeholder="Add meg a Jelszavad" name="password" value="">
+                        <input type="password" class="form-control" placeholder="Add meg a Jelszavad" name="password" value="{{old('password')}}">
                         <span class="text-danger">@error('password') {{$message}} @enderror</span>
                     </div>
 
                     <div class="form-group">
                         <label for="lastname">Vezetéknév</label>
-                        <input type="text" class="form-control" placeholder="Add meg a Vezetékneved" name="lastname" value="">
+                        <input type="text" class="form-control" placeholder="Add meg a Vezetékneved" name="lastname" value="{{old('lastname')}}">
                         <span class="text-danger">@error('lastname') {{$message}} @enderror</span>
                     </div>
 
-                    
+
                     <div class="form-group">
                         <label for="firstname">Keresztnév</label>
-                        <input type="text" class="form-control" placeholder="Add meg a Keresztneved" name="firstname" value="">
+                        <input type="text" class="form-control" placeholder="Add meg a Keresztneved" name="firstname" value="{{old('firstname')}}">
                         <span class="text-danger">@error('firstname') {{$message}} @enderror</span>
                     </div>
 
                     <div class="form-group">
                         <label for="birthdate">Születési idő</label>
-                        <input type="date" class="form-control" placeholder="Add meg a Születési időd" name="birthdate" value="">
+                        <input type="date" class="form-control" placeholder="Add meg a Születési időd" name="birthdate" value="{{old('birthdate')}}">
                         <span class="text-danger">@error('birthdate') {{$message}} @enderror</span>
                     </div>
 
                     <div class="form-group">
                         <label for="birthplace">Születési hely</label>
-                        <input type="text" class="form-control" placeholder="Add meg a Születési helyed" name="birthplace" value="">
+                        <input type="text" class="form-control" placeholder="Add meg a Születési helyed" name="birthplace" value="{{old('birthplace')}}">
                         <span class="text-danger">@error('birthplace') {{$message}} @enderror</span>
                     </div>
 
                     <div class="form-group">
-                        <label for="city">Lakhely</label>
-                        <input type="text" class="form-control" placeholder="Add meg a Lakhelyed" name="city" value="">
+                        <label for="city">Város</label>
+                        <input type="text" class="form-control" placeholder="Add meg a Várost" name="city" value="{{old('city')}}">
                         <span class="text-danger">@error('city') {{$message}} @enderror</span>
                     </div>
 
-                    
+
                     <div class="form-group">
                         <label for="postalcode">Irányítószám</label>
-                        <input type="number" class="form-control" placeholder="Add meg az Irányítószámod" name="postalcode" value="">
+                        <input type="number" class="form-control" placeholder="Add meg az Irányítószámod" name="postalcode" value="{{old('postalcode')}}">
                         <span class="text-danger">@error('postalcode') {{$message}} @enderror</span>
                     </div>
 
                     <div class="form-group">
                         <label for="adress">Cím</label>
-                        <input type="text" class="form-control" placeholder="Add meg a Címed" name="adress" value="">
+                        <input type="text" class="form-control" placeholder="Add meg a Címed" name="adress" value="{{old('adress')}}">
                         <span class="text-danger">@error('adress') {{$message}} @enderror</span>
                     </div>
 
                     <div class="form-group">
-                     <button class="btn btn-block btn-warning" type="submit">Regisztráció</button>
+                        <button class="btn btn-block btn-warning" type="submit">Regisztráció</button>
                     </div>
                     <br>
                     <a href="login">Ha már van regisztrációd katt ide!</a>
